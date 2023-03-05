d1bece7ae846616749f469bd760e53b5ddf248a3
Jonathan Cheng <jcheng461@gmail.com>
2023-03-05 17:28:29 +0900

    set up namespaces and business logic

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -1,7 +1,19 @@
-class Program
+using System;
+using ShapeTracker.Models;
+
+namespace ShapeTracker
 {
-  static void Main()
+  class Program
   {
-    
+    static void Main()
+    {
+      Triangle testTriangle = new Triangle(3,4,5);
+      Console.WriteLine($"Side one is: {testTriangle.Side1}");
+      Console.WriteLine($"Side two is: {testTriangle.Side2}");
+      Console.WriteLine($"Side three is: {testTriangle.Side3}");
+      Console.WriteLine("Calculating type of triangle..");
+      Console.WriteLine($"Triangle type: {testTriangle.CheckType()}.");
+    }
   }
 }
+
