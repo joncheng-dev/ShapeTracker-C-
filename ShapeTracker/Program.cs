7df4565262b4bfa1126a009b04f8eb413ab17ef0
Jonathan Cheng <jcheng461@gmail.com>
2023-03-05 22:03:19 +0900

    change access modifier for fields in business logic Triangle class. Use getter and setter methods to access fields

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -8,11 +8,17 @@ namespace ShapeTracker
     static void Main()
     {
       Triangle testTriangle = new Triangle(3,4,5);
-      Console.WriteLine($"Side one is: {testTriangle.Side1}");
-      Console.WriteLine($"Side two is: {testTriangle.Side2}");
-      Console.WriteLine($"Side three is: {testTriangle.Side3}");
+      Console.WriteLine($"Side one is: {testTriangle.GetSide1()}");
+      Console.WriteLine($"Side two is: {testTriangle.GetSide2()}");
+      Console.WriteLine($"Side three is: {testTriangle.GetSide3()}");
       Console.WriteLine("Calculating type of triangle..");
       Console.WriteLine($"Triangle type: {testTriangle.CheckType()}.");
+      Console.WriteLine("Updating sides...");
+      testTriangle.SetSide1(44);
+      testTriangle.SetSide2(44);
+      testTriangle.SetSide3(70);
+      Console.WriteLine($"The triangle's sides are now {testTriangle.GetSide1()}, {testTriangle.GetSide2()}, and {testTriangle.GetSide3()}");
+      Console.WriteLine($"Now the triangle is: {testTriangle.CheckType()}.");
     }
   }
 }
