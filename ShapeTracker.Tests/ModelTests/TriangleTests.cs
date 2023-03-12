be317a7b7a477d7b0a3b07fc50e02943fdb3594f
Jonathan Cheng <jcheng461@gmail.com>
2023-03-12 10:33:20 +0900

    add passing test, code for Triangle.Side2 SET

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -39,5 +39,14 @@ namespace ShapeTracker.Tests
       int result = newTriangle.Side2;
       Assert.AreEqual(length2, result);
     }
+
+    [TestMethod]
+    public void SetSide2_SetValueOfSide2_Void()
+    {
+      Triangle newTriangle = new Triangle(3, 4);
+      int newLength2 = 6;
+      newTriangle.Side2 = newLength2;
+      Assert.AreEqual(newLength2, newTriangle.Side2);
+    }
   }
 }
