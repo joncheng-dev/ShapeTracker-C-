4942444b24f6a79972717f6444f80bed4bb48421
Jonathan Cheng <jcheng461@gmail.com>
2023-03-13 22:17:08 +0900

    for Rectangle business logic -- add Side1 get functionality, and passes test

--- ShapeTracker.Tests/ModelTests/RectangleTests.cs
+++ ShapeTracker.Tests/ModelTests/RectangleTests.cs
@@ -11,8 +11,17 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void RectangleConstructor_CreatesInstanceOfRectangle_Rectangle()
     {
-      Rectangle newRectangle = new Rectangle();
+      Rectangle newRectangle = new Rectangle(3);
       Assert.AreEqual(typeof(Rectangle), newRectangle.GetType());
+    }
+
+    [TestMethod]
+    public void GetSide1_ReturnsSide1_Int()
+    {
+      int length1 = 4;
+      Rectangle newRectangle = new Rectangle(length1);
+      int result = newRectangle.Side1;
+      Assert.AreEqual(length1, result);
     }
   }
 }
