a1f1bf97a24773427271c0403a3feb497f1628b0
Jonathan Cheng <jcheng461@gmail.com>
2023-03-15 23:32:41 +0900

    WIP: update UI to allow user to select triangles or rectangles

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -10,6 +10,12 @@ namespace ShapeTracker
     {
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("Welcome to the Shape Tracker App!");
+      UserChoosesTriangle();
+    }
+
+    static void UserChoosesTriangle()
+    {
+      Console.WriteLine("You have chosen 'triangle'");
       Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
 
       Console.WriteLine("Enter length of 1st side:");
