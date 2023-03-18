63a79eccca811f504e9ec62afc7d2df36bfca0b3
Jonathan Cheng <jcheng461@gmail.com>
2023-03-18 11:23:25 +0900

    add Side1 get functionality and pass test

--- ShapeTracker.Tests/ModelTests/EllipseTests.cs
+++ ShapeTracker.Tests/ModelTests/EllipseTests.cs
@@ -14,5 +14,14 @@ namespace ShapeTracker.Tests
       Ellipse newEllipse = new Ellipse(1, 4);
       Assert.AreEqual(typeof(Ellipse), newEllipse.GetType());
     }
+
+    [TestMethod]
+    public void GetSide1_ReturnsSide1_Int()
+    {
+      int length1 = 2;
+      Ellipse newEllipse = new Ellipse(length1, 3);
+      int result = newEllipse.Side1;
+      Assert.AreEqual(length1, result);
+    }
   }
 }
--- ShapeTracker/Models/Ellipse.cs
+++ ShapeTracker/Models/Ellipse.cs
@@ -2,9 +2,16 @@ namespace ShapeTracker.Models
 {
   public class Ellipse
   {
+    private int _side1;
+
+    public int Side1
+    {
+      get { return _side1; }
+    }
+
     public Ellipse(int length1, int length2)
     {
-
+      _side1 = length1; 
     }
   }
 }
