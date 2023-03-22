f40db10245882dbcdd56d7f5bed94e42876b687b
Jonathan Cheng <jcheng461@gmail.com>
2023-03-22 14:32:16 +0900

    update UI to include ellipses. DRY up code for duplicate methods in Program.cs

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -10,7 +10,7 @@ namespace ShapeTracker
     {
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("Welcome to the Shape Tracker App!");
-      Console.WriteLine("For triangles, enter '1'. For rectangles, enter '2'.");
+      Console.WriteLine("For triangles, ..enter '1'.\nFor rectangles, .enter '2'.\nFor ellipses, ...enter '3'.\nTo exit program, type 'exit'.");
       string userResponse = Console.ReadLine();
 
       if (userResponse == "1")
@@ -21,9 +21,17 @@ namespace ShapeTracker
       {
         UserChoosesRectangle();
       } 
+      else if (userResponse == "3")
+      {
+        UserChoosesEllipse();
+      }
+      else if (userResponse == "exit")
+      {
+        Console.WriteLine("Goodbye.");
+      }
       else 
       {
-        Console.WriteLine("For triangles, enter '1'. For rectangles, enter '2'.");
+        Console.WriteLine("For triangles, ..enter '1'.\nFor rectangles, .enter '2'.\nFor ellipses, ...enter '3'.");
       }
 
     }
@@ -64,13 +72,27 @@ namespace ShapeTracker
 
       Rectangle rect = new Rectangle(length1, length2);
 
-      int result = CalculateRectangleArea(rect);
+      int result = rect.CalculateRectangleArea();
       Console.WriteLine($"Your rectangle's area is {result}.");
     }
 
-    static int CalculateRectangleArea(Rectangle rect)
+    static void UserChoosesEllipse()
     {
-      return (rect.Side1 * rect.Side2);
+      Console.WriteLine("You have chosen 'ellipse'");
+      Console.WriteLine("We'll calculate the area of an ellipse based on the lengths of its two radii.");
+
+      Console.WriteLine("Enter length of 1st radius:");
+      string strNumber1 = Console.ReadLine();
+      Console.WriteLine("Enter length of 2nd radius:");
+      string strNumber2 = Console.ReadLine();
+
+      int length1 = int.Parse(strNumber1);
+      int length2 = int.Parse(strNumber2);
+
+      Ellipse ell = new Ellipse(length1, length2);
+
+      double result = ell.CalculateEllipseArea();
+      Console.WriteLine($"Your ellipse's area is {result}.");
     }
 
     static void ConfirmOrEditTriangle(Triangle tri)
@@ -80,9 +102,7 @@ namespace ShapeTracker
       Console.WriteLine($"Side 2 has a length of {tri.Side2}.");
       Console.WriteLine($"Side 3 has a length of {tri.GetSide3()}.");
       Console.WriteLine("Is that correct? Enter 'yes' to proceed, or 'no' to re-enter the triangle's sides");
-      
       string userInput = Console.ReadLine();
-
 
       if (userInput == "yes")
       {
